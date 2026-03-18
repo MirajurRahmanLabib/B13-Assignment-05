e7b5868bd5a5d45c023d1c863ef5ef28906ca9bd
MirajurRahmanLabib <mirajur.r.labib@gmail.com>
2026-03-18 11:55:57 +0600

    modal done

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -3,8 +3,29 @@ const createElements =(arr) =>{
     return htmlElements.join(' ');
 };
 
+const issueCounter = () =>{
+    const totalIssues = document.getElementById('total-issues');
+    console.log(document.getElementById('issue-container').children)
+    totalIssues.innerText = document.getElementById('issue-container').children.length;
+};
+
+const mngLoadingBar=(status)=>{
+    if(status == true){
+        document.getElementById("loading-bar").classList.remove('hidden')
+        document.getElementById("issue-container").classList.add('hidden')
+    }
+    else{
+        document.getElementById("issue-container").classList.remove('hidden')
+        document.getElementById("loading-bar").classList.add('hidden')
+    }
+};
+
+
+
+
 
 const loadIssueCards=()=>{
+    mngLoadingBar(true);
 const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`;
     fetch(url)
     .then(res => res.json())
@@ -18,6 +39,7 @@ const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`;
     document.getElementById('all').classList.remove('btn-outline')
 };
 const loadIssueCardsOpen=()=>{
+    mngLoadingBar(true);
 const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`;
     fetch(url)
     .then(res => res.json())
@@ -31,6 +53,7 @@ const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`;
     document.getElementById('open').classList.remove('btn-outline')
 };
 const loadIssueCardsClosed=()=>{
+    mngLoadingBar(true);
 const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`;
     fetch(url)
     .then(res => res.json())
@@ -45,6 +68,10 @@ const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`;
     document.getElementById('closed').classList.remove('btn-outline')
 };
 
+
+
+
+
 const displayIssueCards = (issues) => {
     const issueContainer = document.getElementById('issue-container');
     issueContainer.innerHTML = '';
@@ -52,7 +79,7 @@ const displayIssueCards = (issues) => {
     for(let issue of issues){
         const issueDiv = document.createElement('div')
         issueDiv.innerHTML = `
-            <div class="issue-card rounded-[4px] bg-base-100 shadow-md p-4 border-t-3 ${issue.status == "open" ? `border-[#00A96E]` :
+            <div onclick="loadIssueDetail(${issue.id})" class="issue-card rounded-[4px] bg-base-100 shadow-md p-4 border-t-3 ${issue.status == "open" ? `border-[#00A96E]` :
                 `border-[#A855F7]`} space-y-3 ">
                 <div class="flex justify-between items-center">
                     <div class="">${issue.status == "open" ? `
@@ -73,16 +100,9 @@ const displayIssueCards = (issues) => {
             </div>   
         `
         issueContainer.append(issueDiv)
-    }  
-}
-loadIssueCards();
-
-
-const issueCounter = () =>{
-    const totalIssues = document.getElementById('total-issues');
-    console.log(document.getElementById('issue-container').children)
-    totalIssues.innerText = document.getElementById('issue-container').children.length;
-}
+    }
+    mngLoadingBar(false);
+};
 
 const displayIssueCardsOpen = (issues) => {
     const issueContainer = document.getElementById('issue-container');
@@ -93,7 +113,7 @@ const displayIssueCardsOpen = (issues) => {
 
             const issueDiv = document.createElement('div')
             issueDiv.innerHTML = `
-            <div class="issue-card rounded-[4px] bg-base-100 shadow-md p-4 border-t-3 ${issue.status == "open" ? `border-[#00A96E]` :
+            <div onclick="loadIssueDetail(${issue.id})" class="issue-card rounded-[4px] bg-base-100 shadow-md p-4 border-t-3 ${issue.status == "open" ? `border-[#00A96E]` :
                 `border-[#A855F7]`} space-y-3 ">
                 <div class="flex justify-between items-center">
                 <div class="">${issue.status == "open" ? `
@@ -114,9 +134,11 @@ const displayIssueCardsOpen = (issues) => {
                     </div>   
                     `
                     issueContainer.append(issueDiv)
-                }
-            }  
         }
+    }
+    mngLoadingBar(false);  
+};
+
 const displayIssueCardsClosed = (issues) => {
     const issueContainer = document.getElementById('issue-container');
     issueContainer.innerHTML = '';
@@ -126,7 +148,7 @@ const displayIssueCardsClosed = (issues) => {
 
             const issueDiv = document.createElement('div')
             issueDiv.innerHTML = `
-            <div class="issue-card rounded-[4px] bg-base-100 shadow-md p-4 border-t-3 ${issue.status == "open" ? `border-[#00A96E]` :
+            <div onclick="loadIssueDetail(${issue.id})" class="issue-card rounded-[4px] bg-base-100 shadow-md p-4 border-t-3 ${issue.status == "open" ? `border-[#00A96E]` :
                 `border-[#A855F7]`} space-y-3 ">
                 <div class="flex justify-between items-center">
                 <div class="">${issue.status == "open" ? `
@@ -147,6 +169,56 @@ const displayIssueCardsClosed = (issues) => {
                     </div>   
                     `
                     issueContainer.append(issueDiv)
-                }
-            }  
         }
+    }
+    mngLoadingBar(false);
+};
+
+
+
+loadIssueCards();
+
+
+const loadIssueDetail=(id)=>{
+const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayIssueDetail(data.data));
+};
+
+const displayIssueDetail = (issue) =>{
+    const detailsContainer = document.getElementById('details-container');
+    detailsContainer.innerHTML =`
+            <div class="issue-card rounded-[4px] bg-base-100 shadow-md p-4 border-t-3 ${issue.status == "open" ? `border-[#00A96E]` :
+                `border-[#A855F7]`} space-y-3 ">
+                <div class="flex justify-between items-center">
+                <div class="">${issue.status == "open" ? `
+                    <img class="" src="./assets/Open-Status.png" alt=""> Open` : `<img class="" src="./assets/Closed-Status.png" alt=""> close`}
+                    
+                    </div>
+                    <div class="btn btn-soft ${issue.priority == 'high' ? `btn-error`: issue.priority == 'medium' ? `btn-warning` : ``} rounded-full h-full">${issue.priority}</div>
+                    </div>
+                    <div><h2 class="font-semibold text-sm">${issue.title}</h2>
+                    <p class="text-[#64748B] text-xs line-clamp-2">${issue.description}</p>
+                    </div>
+                    <div>${createElements(issue.labels)}</div>
+                    <hr class="border-slate-200">
+                    <div class="text-[#64748B] text-xs space-y-2">
+                    <p>#1 by ${issue.author}</p>
+                    <p> Assignee : ${issue.assignee}</p>
+                    <p> Created : ${issue.createdAt}</p>
+                    <p> Updated : ${issue.updatedAt}</p>
+                    </div>
+                </div> 
+                <form method="dialog" class="flex justify-end mt-3">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn btn-primary">Close</button>
+                </form>
+            </div>
+    `
+    
+    my_modal_5.showModal();
+};
+
+
+
